fa119b653ce1549133b008878d2918a8e3d183f4
Lokhia <chloe.daudier@hotmail.fr>
2022-05-31 22:41:00 +0200

    Visualization of age gap according to the gender of the oldest

--- notebooks/age_gap/hollywood_age_gap_visualization.py
+++ notebooks/age_gap/hollywood_age_gap_visualization.py
@@ -21,8 +21,16 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Écarts d\'âge')
 hist_values = np.histogram(data['Age Difference'], bins=10)[0]
-hist3 = np.histogram(data['Age Difference'], bins=20)[0]
-st.bar_chart(hist_values)
+actor1_older = data[data['Actor 1 Age']>data['Actor 2 Age']]
+actor2_older = data[data['Actor 1 Age']<data['Actor 2 Age']]
+older_woman = pd.concat([actor1_older[actor1_older['Actor 1 Gender'] == 'woman'], actor2_older[actor2_older['Actor 2 Gender'] == 'woman']])
+older_man = pd.concat([actor1_older[actor1_older['Actor 1 Gender'] == 'man'], actor2_older[actor2_older['Actor 2 Gender'] == 'man']])
+
+st.write(older_woman.shape)
+st.write(older_man.shape)
+
+st.bar_chart(np.histogram(older_man['Age Difference'], bins=20)[0])
+st.bar_chart(np.histogram(older_woman['Age Difference'], bins=20)[0])
 
 #
 # st.subheader('Number of pickups by hour')
