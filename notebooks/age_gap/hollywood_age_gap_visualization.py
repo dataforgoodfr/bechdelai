bab4e418745cbe174b3f83221ef7a1bcb0b4fd5d
Lokhia <chloe.daudier@hotmail.fr>
2022-06-01 15:05:22 +0200

    Condensed histogram of age gap and file cleaning

--- notebooks/age_gap/hollywood_age_gap_visualization.py
+++ notebooks/age_gap/hollywood_age_gap_visualization.py
@@ -1,45 +1,43 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import plotly.express as px
 
-DATA_URL = ('http://hollywoodagegap.com/movies.csv')
+DATA_URL = 'http://hollywoodagegap.com/movies.csv'
+
 
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    return data
+def load_data():
+    return pd.read_csv(DATA_URL)
 
 
-st.title('Hollywood Age Gap')
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+def define_the_oldest(df):
+    gender_list = []
+    for index, row in df.iterrows():
+        if row["Actor 1 Age"] > row["Actor 2 Age"]:
+            gender_list.append(row["Actor 1 Gender"])
+        elif row["Actor 2 Age"] > row["Actor 1 Age"]:
+            gender_list.append(row["Actor 2 Gender"])
+        else:
+            gender_list.append("same")
+    return gender_list
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
 
-st.subheader('Écarts d\'âge')
-hist_values = np.histogram(data['Age Difference'], bins=10)[0]
-actor1_older = data[data['Actor 1 Age']>data['Actor 2 Age']]
-actor2_older = data[data['Actor 1 Age']<data['Actor 2 Age']]
-older_woman = pd.concat([actor1_older[actor1_older['Actor 1 Gender'] == 'woman'], actor2_older[actor2_older['Actor 2 Gender'] == 'woman']])
-older_man = pd.concat([actor1_older[actor1_older['Actor 1 Gender'] == 'man'], actor2_older[actor2_older['Actor 2 Gender'] == 'man']])
+def main():
+    st.set_page_config(layout="wide")
+    st.title('Hollywood Age Gap')
+    raw_data = load_data()
 
-st.write(older_woman.shape)
-st.write(older_man.shape)
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(raw_data)
 
-st.bar_chart(np.histogram(older_man['Age Difference'], bins=20)[0])
-st.bar_chart(np.histogram(older_woman['Age Difference'], bins=20)[0])
+    st.subheader('Age Gap Visualization')
+    data = raw_data.copy()
+    data["Is_hetero"] = data["Actor 2 Gender"] != data["Actor 1 Gender"]
+    data["Gender of the oldest"] = define_the_oldest(data)
+    fig = px.histogram(data, x="Age Difference", color="Gender of the oldest", marginal="box", hover_data=data.columns)
+    st.plotly_chart(fig, use_container_width=True)
 
-#
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-#
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-#
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
+
+if __name__ == "__main__":
+    main()
