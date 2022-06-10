567a8eddce82bfe63a98b798d608fb369d68d882
JPegoraro <juliana.a.pegoraro@gmail.com>
2022-06-10 22:50:11 +0200

    print couple details and correct characters order

--- notebooks/age_gap/relationships_visualisation.py
+++ notebooks/age_gap/relationships_visualisation.py
@@ -18,13 +18,13 @@ MOVIE_YEARS = {"Harry Potter and the Goblet of Fire":2005,
               "The Big Lebowski":1998,
               "Love Actually":2003}
 
+VERBS = ['kisses', 'sleeps with', 'goes on a date with', 'has sex with', 'marries', 'is in love with','is in couple with', 'is the father of', 'is the mother of']
 
 @st.cache
 def load_data_from_file(file):
     return pd.read_csv(file)
 def load_data(movie):
-    verbs = ['kisses', 'sleeps with', 'goes on a date with', 'has sex with', 'marries', 'is in love with','is in couple with', 'is the father of', 'is the mother of']
-    return pc.compute_relationships_in_movie(movie.cast,movie.plot, verbs)
+    return pc.compute_relationships_in_movie(movie.cast,movie.plot, VERBS)
 
 
 def main():
@@ -41,21 +41,25 @@ def main():
     count=0
     for i,row in scores.sort_values('score',ascending=False).iterrows():
 
+        if row.question not in VERBS:
+            continue
+
         if (count==10) | (row.score<0.3):
             break
 
         if ('mother' in row.question) | ('father' in row.question): # for parental relationship, the threshold is high
-            if row.score < 0.9:
+            if row.score < 0.85:
                 continue
-        st.subheader('{} {} {}'.format(row.character1, row.question, row.character2))
-        st.write(row.score)
+        st.subheader('{} {} {}'.format(row.character2, row.question, row.character1))
+        st.write('Score: ',row.score)
+        st.write('Age gap: ' ,row.age_gap)
 
         col1, col2, col3,col4 = st.columns([3,3,2,10])
-        img_star1 = tmdb.get_person_image_from_id(row.star_id1)
-        col1.image('https://image.tmdb.org/t/p/original'+img_star1["profiles"][0]["file_path"],width=200)
-
         img_star2 = tmdb.get_person_image_from_id(row.star_id2)
-        col2.image('https://image.tmdb.org/t/p/original'+img_star2["profiles"][0]["file_path"],width=200)
+        col1.image('https://image.tmdb.org/t/p/original'+img_star2["profiles"][0]["file_path"],width=200)
+
+        img_star1 = tmdb.get_person_image_from_id(row.star_id1)
+        col2.image('https://image.tmdb.org/t/p/original'+img_star1["profiles"][0]["file_path"],width=200)
 
         relationship_true = col3.radio('Is this relationship true?', ['Yes', 'No'],key = "couple_"+str(i))
         count+=1
