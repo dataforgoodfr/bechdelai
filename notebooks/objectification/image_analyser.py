6c0319e21f53f677b31c077a8443151afea6d0b3
sonicpierre <pvirgaux@gmail.com>
2022-08-14 18:51:05 +0200

    Merging

--- notebooks/objectification/image_analyser.py
+++ notebooks/objectification/image_analyser.py
@@ -4,8 +4,8 @@ sys.path.append('/home/virgaux/dataforgood/bechdelai/bechdelai')
 
 from model import Classifier, Detector
 
-Classifier("/inputs/nudeModel/model/classifier_model.onnx")
-Detector("/inputs/nudeModel/model/detector_v2_default_checkpoint.onnx", "/inputs/nudeModel/model/detector_v2_default_classes")
+classifier = Classifier("/inputs/nudeModel/model/classifier_model.onnx")
+detector = Detector("/inputs/nudeModel/model/detector_v2_default_checkpoint.onnx", "/inputs/nudeModel/model/detector_v2_default_classes")
 
 res_detector = detector.make_pred('/inputs/bechdelia/exemple_2.jpeg')
 print(res_detector)
