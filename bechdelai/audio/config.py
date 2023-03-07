c620f9a09cc1a1045a7bb0d85d04df737543fdd9
Lokhia <chloe.daudier@hotmail.fr>
2023-03-07 15:39:11 +0100

    Include Us English profile and tutorial

--- bechdelai/audio/config.py
+++ bechdelai/audio/config.py
@@ -2,11 +2,14 @@ from . import profiles
 
 
 class Config:
-    selected_profile: profiles.Profiles
+    def __init__(self):
+        self.selected_profile: profiles.Profiles
 
     def select_profile(self, option):
         if option == "FR":
             self.selected_profile = profiles.French()
+        elif option == "US":
+            self.selected_profile = profiles.USEnglish()
 
     def get_profile(self):
         return self.selected_profile
--- bechdelai/audio/profiles.py
+++ bechdelai/audio/profiles.py
@@ -1,3 +1,5 @@
+""" Profiles are used to set the adapted configuration to analyse the video of interest.
+"""
 from abc import ABC, abstractmethod
 
 from . import transcriber, gender_segmenter, dialogue_tagger
@@ -27,3 +29,13 @@ class French(Profiles):
     def get_transcriber(self):
         return transcriber.GoogleSpeechRecognition("fr-FR")
 
+
+class USEnglish(Profiles):
+    def get_gender_segmentor(self):
+        return gender_segmenter.InaSpeechSegmentor()
+
+    def get_dialogue_tagger(self):
+        return dialogue_tagger.RuleBasedTagger()
+
+    def get_transcriber(self):
+        return transcriber.GoogleSpeechRecognition("en-US")
